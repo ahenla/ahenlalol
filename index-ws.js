8f59f2d2c3e4024247f746ab8b165f100e2f4fec
Andres Henriquez <rymbell@gmail.com>
2023-11-22 10:57:09 +0900

    added database

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,14 @@ server.listen(3000, function(){
   console.log('server started on port 3000');
 });
 
+process.on('SIGINT', () => {
+  wss.clients.forEach( function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  })
+});
 
 /** Websocket start **/ 
 const WebSocketServer = require('ws').Server;
@@ -27,6 +35,10 @@ wss.on('connection', function connection(ws) {
     ws.send('Welcome to my server')
   };
 
+  db.run(`INSERT INTO visitors (count, time)
+          VALUES (${numClients}, datetime('now'))
+        `);
+
   ws.on('close', function close(){
     wss.broadcast(`Current visitors ${numClients}`);
     console.log('A client has disconnected'); 
@@ -38,3 +50,32 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/** end web sockets **/ 
+
+/** database start **/ 
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `)
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  })
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("shutting down database");
+  db.close();
+}
+
+
